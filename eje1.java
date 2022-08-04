0a86c23aa68d64a89587a6bfd657abb8d920ea3d
Andres Chavez <107895172+ChavitoCdm@users.noreply.github.com>
2022-08-04 18:01:22 -0300

    Add files via upload

---
+++ eje1.java
@@ -0,0 +1,20 @@
+package Tp34;
+
+public class eje1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Declarar una variable de tipo cadena (String) y una variable de tipo entero, asignar a la
+		//variable string tu nombre y a la variable entera tu edad y mostrar el mensaje "Mi nombre
+		//es...y mi edad es..."
+		String name = "Andres ";
+		String sun = "Chavez ";
+		int edad = 33;
+		
+		System.out.println("hola mi nombre es " +  name  +  sun + "TENGO " + edad + " Años" );
+		// System.out.println("tengo " + edad + " AÑOS ");
+			
+		
+	}
+
+}
